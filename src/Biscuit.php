2398c96123e7840b3dc109bcb03f924b3359cd7e
Rubén Martín <hasdpk@gmail.com>
2021-01-20 04:30:10 +0000

    Crear la clase Biscuit para la gestión de la Cookie

---
+++ src/Biscuit.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bazaarya\Toolbar;
+
+use Cookie;
+
+class Biscuit
+{
+    const COOKIE_NAME = 'bzry_toolbar';
+
+    /**
+     * @var Cookie
+     */
+    private $cookie;
+
+    protected function get(): Cookie
+    {
+        if ($this->cookie instanceof Cookie) {
+            return $this->cookie;
+        }
+
+        return $this->cookie = new Cookie('psAdmin');
+    }
+
+    public function retrieve(): array
+    {
+        $links = (string) $this->get()->{self::COOKIE_NAME};
+        $links = json_decode($links, true);
+
+        return (array) $links;
+    }
+
+    public function store(array $links): void
+    {
+        $this->get()->{self::COOKIE_NAME} = json_encode($links);
+        $this->get()->write();
+    }
+}
